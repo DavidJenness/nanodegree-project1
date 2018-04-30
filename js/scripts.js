741d3cc8dde90c4b39e4a8afba9698d5bd0a508b
David4FSU <david4fsu@hotmail.com>
2018-04-30 08:26:59 -0500

    Code Cleanup

--- js/scripts.js
+++ js/scripts.js
@@ -11,7 +11,7 @@ $(document).ready(function () {
                 $("#centerProjectName").text("Database Dev");
                 $("#rightProjectName").text("Database Dev");
 
-                $("#rightPicLink").attr("href","/overview.html#Database")
+                $("#rightPicLink").attr("href", "/overview.html#Database")
 
                 break;
             case 'iOS':
@@ -19,21 +19,29 @@ $(document).ready(function () {
                 $("#centerProjectName").text("iOS App #2");
                 $("#rightProjectName").text("iOS Overview");
 
-                $("#rightPicLink").attr("href","/overview.html#iOS")
+                $("#leftPic").attr("src", "img/left/ios.png");
+                $("#centerPic").attr("src", "img/center/iOS2.png");
+                $("#rightPic").attr("src", "img/right/xcode.jpg");
+
+                $("#leftPicLink").attr("href", "https://itunes.apple.com/us/app/redcritter/id791210761?mt=8")
+                $("#leftDescription").attr("href", "https://itunes.apple.com/us/app/redcritter/id791210761?mt=8")
+                $("#leftDescription").text("View iOS App")
+
+                $("#rightPicLink").attr("href", "/overview.html#iOS")
                 break;
             case "Android":
                 $("#leftProjectName").text("Android App #1");
                 $("#centerProjectName").text("Android App #2");
                 $("#rightProjectName").text("Android Overview");
 
-                $("#rightPicLink").attr("href","/overview.html#Android")
+                $("#rightPicLink").attr("href", "/overview.html#Android")
                 break;
             case "Cloud":
                 $("#leftProjectName").text("Amazon AWS Dev");
                 $("#centerProjectName").text("Microsoft Azure Dev");
                 $("#rightProjectName").text("Cloud Overview");
 
-                $("#rightPicLink").attr("href","/overview.html#Cloud")
+                $("#rightPicLink").attr("href", "/overview.html#Cloud")
                 break;
 
             case "GIS":
@@ -41,7 +49,7 @@ $(document).ready(function () {
                 $("#centerProjectName").text("GIS Project #2");
                 $("#rightProjectName").text("GIS Overview");
 
-                $("#rightPicLink").attr("href","/overview.html#GIS")
+                $("#rightPicLink").attr("href", "/overview.html#GIS")
                 break;
 
             case "Web":
@@ -49,30 +57,30 @@ $(document).ready(function () {
                 $("#centerProjectName").text("Web Project #2");
                 $("#rightProjectName").text("Web Overview");
 
-                $("#rightPicLink").attr("href","/overview.html#Web")
+                $("#rightPicLink").attr("href", "/overview.html#Web")
                 break;
             case "Certified":
                 $("#leftProjectName").text("Amazon AWS");
                 $("#centerProjectName").text("Microsoft");
                 $("#rightProjectName").text("Certifications Overview");
 
-                $("#leftPic").attr("src","img/left/AWS.png");
-                $("#centerPic").attr("src","img/center/mcse.gif");
-                $("#rightPic").attr("src","img/right/Cert.png");
+                $("#leftPic").attr("src", "img/left/AWS.png");
+                $("#centerPic").attr("src", "img/center/mcse.gif");
+                $("#rightPic").attr("src", "img/right/Cert.png");
 
-                $("#leftPicLink").attr("href","https://www.certmetrics.com/amazon/public/transcript.aspx?transcript=3DNKSDEKCNR1QZGW")
-                $("#leftDescription").attr("href","https://www.certmetrics.com/amazon/public/transcript.aspx?transcript=3DNKSDEKCNR1QZGW")
+                $("#leftPicLink").attr("href", "https://www.certmetrics.com/amazon/public/transcript.aspx?transcript=3DNKSDEKCNR1QZGW")
+                $("#leftDescription").attr("href", "https://www.certmetrics.com/amazon/public/transcript.aspx?transcript=3DNKSDEKCNR1QZGW")
                 $("#leftDescription").text("View AWS Transcript")
 
                 // Since it is a PDF, let's open it in a new tab
-                $("#centerDescription").attr("target","_blank")
-                $("#centerPicLink").attr("target","_blank")
-                $("#centerPicLink").attr("href","https://s3-us-west-2.amazonaws.com/udacity-jenness/MS_Learning_Transcript.PDF")
-                $("#centerDescription").attr("href","https://s3-us-west-2.amazonaws.com/udacity-jenness/MS_Learning_Transcript.PDF")
+                $("#centerDescription").attr("target", "_blank")
+                $("#centerPicLink").attr("target", "_blank")
+                $("#centerPicLink").attr("href", "https://s3-us-west-2.amazonaws.com/udacity-jenness/MS_Learning_Transcript.PDF")
+                $("#centerDescription").attr("href", "https://s3-us-west-2.amazonaws.com/udacity-jenness/MS_Learning_Transcript.PDF")
                 $("#centerDescription").text("View Microsoft Transcript")
 
-                $("#rightPicLink").attr("href","/overview.html#Certified")
-                $("#rightDescription").attr("href","/overview.html#Certified")
+                $("#rightPicLink").attr("href", "/overview.html#Certified")
+                $("#rightDescription").attr("href", "/overview.html#Certified")
                 $("#rightDescription").text("Click for info")
                 break;
             default:
